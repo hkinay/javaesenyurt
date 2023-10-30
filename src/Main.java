755b4a61ac41f9a332123ccac8d18b0c5b9a6684
hkinay <kinayhuseyin@gmail.com>
2023-10-30 15:44:02 +0300

    ilk deneme

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Bu kısım yorum kısmı
+        //Değişkenler - Variables
+        int a;
+        String ad = "Hüseyin";
+        double sayi = 20.35;
+        a = 5;
+
+        System.out.println("Merhaba Dünya");
+        System.out.println(ad);
+        System.out.println(sayi);
+        System.out.println(a);
+
+
+
+    }
+}
